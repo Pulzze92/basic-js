0ad98361b80ff063f7af508f077a5441ec7dc199
Pulzze92 <89998310392@mail.ru>
2021-09-18 23:23:40 +0300

    feat: add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -7,19 +7,31 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 
 export default {
+  arr: [],
   getLength() {
-
+  return this.arr.length;
   },
   addLink(value) {
-
+  this.arr.push(value);
+  return this;
   },
   removeLink(position) {
-
+  if(!position || position < 0 || typeof position != 'number' || position >= this.arr.length) {
+    this.arr = [];
+    throw new Error('You can\'t remove incorrect link!');
+  }
+  this.arr.splice(position - 1, 1);
+  return this;
   },
   reverseChain() {
-
+  this.arr.reverse();
+  return this;
   },
   finishChain() {
-
+  let res = this.chain.map(function(item) {
+    return `( ${item} )`;
+  });
+  this.arr = [];
+  return res.join('~~');
   }
 };
