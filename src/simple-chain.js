cc03d05c4176d9bf018e4c0f2276502c4f90af2c
Pulzze92 <89998310392@mail.ru>
2021-09-18 23:17:26 +0300

    feat: remove simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,39 +6,20 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 
-let chain = [];
 export default {
   getLength() {
-    return this.chain.length;
+
   },
   addLink(value) {
-    this.chain.push(`( ${value} )`);
-    return this;
+
   },
   removeLink(position) {
-    if(position > this.chain.length - 1) {
-      this.chain = [];
-      throw new Error('message':'You can\'t remove incorrect link!');
-    }
-    if (isNaN(position)) {
-      this.chain = [];
-      throw new Error('message':'You can\'t remove incorrect link!');
-    }
-    if (position < 1) {
-      this.chain = [];
-      throw new Error('message':'You can\'t remove incorrect link!');
-    }
 
-    this.chain.splice(position - 1, 1);
-    return this;
   },
   reverseChain() {
-    this.chain.reverse();
-    return this;
+
   },
   finishChain() {
-    let res = this.chain.join('~~');
-    this.chain = [];
-    return res;
+
   }
 };
